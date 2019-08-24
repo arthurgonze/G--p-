6121e08a8368d69770b44e43ce97a29624cefe97
Arthur Gonze Machado <arthurgonze@gmail.com>
2019-08-24 18:02:01 -0300

    Voces estao prontos marujos ???!!!

---
+++ cmake-build-debug/CMakeFiles/Compilador_2019_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compilador_2019_3.dir/main.c.o"
+  "Compilador_2019_3.pdb"
+  "Compilador_2019_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Compilador_2019_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Vocês estão prontos marujos???!!!\n");
+    return 0;
+}
