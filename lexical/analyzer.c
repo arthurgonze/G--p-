76a752dbfaffa73fdef20a5234537d1791f58a5e
João Souza <joao.souza@ice.ufjf.br>
2019-09-10 17:59:04 -0300

    Complementado automato

--- lexical/analyzer.c
+++ lexical/analyzer.c
@@ -5,6 +5,7 @@
 #include "ctype.h"
 #include "../token.h"
 #include "stdbool.h"
+#include "stdio.h"
 
 #define INITIAL_STATE 0;
 
@@ -13,7 +14,8 @@ int currentState = INITIAL_STATE;
 char currentInput;
 
 int count = 0;
-char* input = "< <= == >= > = != ! asda /";
+//char* input = "< <= == >= > = != ! asda 10.03 10/";
+char* input = "(10+30) (10.1 + .030 - .3e-40)/";
 char getNextChar() {
     currentInput = input[count++];
 }
@@ -38,6 +40,11 @@ bool isDigit(char c) {
     return c >= '0' && c <= '9';
 }
 
+void fail(char* reason) {
+    //TODO jogar os erros para uma pilha
+    printf("Lexicon Error: %s", reason);
+}
+
 int getNextToken() {
 
     while(1) {
@@ -51,6 +58,8 @@ int getNextToken() {
                 else if (isLetter(currentInput)) {
                     getNextCharAndGoTo(13);
                 }
+                else if (isDigit(currentInput))
+                    getNextCharAndGoTo(19);
                 else switch (currentInput) {
                     case '<':
                         getNextCharAndGoTo(1);
@@ -63,6 +72,21 @@ int getNextToken() {
                         break;
                     case '!':
                         getNextCharAndGoTo(10);
+                        break;
+                    case '.':
+                        getNextCharAndGoTo(14);
+                        break;
+                    case '+':
+                        getNextCharAndGoTo(27);
+                        break;
+                    case ')':
+                        getNextCharAndGoTo(28);
+                        break;
+                    case '-':
+                        getNextCharAndGoTo(29);
+                        break;
+                    case '(':
+                        getNextCharAndGoTo(30);
                         break;
                     default:
                         return -1;
@@ -139,9 +163,104 @@ int getNextToken() {
                 else
                     goToState(15);
                 break;
+            case 14:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(55);
+                else
+                    goToState(53);
+                break;
             case 15:
                 goToState(0);
+                //TODO verificar para ver se é palavra reservada
+                //TODO adicionar
                 return ID;
+            case 18:
+                goToState(0);
+                fail("Unexpected character");
+                break;
+            case 19:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(19);
+                else if (currentInput == '.')
+                    getNextCharAndGoTo(25);
+                else if(currentInput == 'e' || currentInput == 'E')
+                    getNextCharAndGoTo(20);
+                else if (isLetter(currentInput))
+                    getNextCharAndGoTo(18);
+                else
+                    goToState(24);
+                break;
+            case 20:
+                if (isDigit(currentInput)) //TODO consertar automato nesse estado
+                    getNextCharAndGoTo(22);
+                else if (currentInput == '+' || currentInput == '-')
+                    getNextCharAndGoTo(21);
+                else
+                    getNextCharAndGoTo(18);
+                break;
+            case 21:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(22);
+                else
+                    getNextCharAndGoTo(18);
+                break;
+            case 22:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(22);
+                else {
+                    goToState(23);
+                }
+                break;
+            case 23:
+                goToState(0);
+                return NUMFLOAT;
+            case 24:
+                goToState(0);
+                return NUMINT;
+            case 25:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(26);
+                else if(isLetter(currentInput))
+                    getNextCharAndGoTo(18);
+                else
+                    goToState(23);
+                break;
+            case 26:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(26);
+                else if (currentInput == 'E' || currentInput == 'e')
+                    getNextCharAndGoTo(20);
+                else if (isLetter(currentInput))
+                    getNextCharAndGoTo(18);
+                else
+                    goToState(23);
+                break;
+            case 27:
+                goToState(0);
+                return PLUS;
+            case 28:
+                goToState(0);
+                return RPARENT;
+            case 29:
+                goToState(0);
+                return MINUS;
+            case 30:
+                goToState(0);
+                return LPARENT;
+            case 53:
+                goToState(0);
+                return DOT;
+            case 55:
+                if(isDigit(currentInput))
+                    getNextCharAndGoTo(55);
+                else if (currentInput == 'e' || currentInput == 'E') //TODO consertar o automato para inserir esse estado
+                    getNextCharAndGoTo(20);
+                else if (isLetter(currentInput)) {
+                    getNextCharAndGoTo(18);
+                } else
+                    //TODO Retornar o valor do número
+                    goToState(23);
+                break;
         }
 
     }
