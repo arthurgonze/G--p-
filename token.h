c4045aed43dfc2c6a86b084d7053a0cfb78cd614
João Souza <joao.souza@ice.ufjf.br>
2019-10-09 20:33:47 -0300

    Unificação da conversão dos tokens no cabeçalho

--- token.h
+++ token.h
@@ -118,7 +118,24 @@ extern "C" {
 #define NUMFLOAT 55
 // LITERAL CHAR
 #define LITERALCHAR 56
-char const *token_id_to_name(int id);
+
+/**
+ * Convert token numerical IDs to a textual represent
+ * @param id Kind of token
+ * @return Textual value of token
+ */
+char const* token_id_to_name(int id) {
+    char const * tokens [] = {"ENDOFILE","LT","LE","EQ","NE","GT","GE","IF","BOOL","ELSE","ID",
+                              "INT","FLOAT","COMMA","LPARENT","RPARENT","ASSIGN","SEMICOLON",
+                              "WHILE","LBRACKET","RBRACKET","SWITCH","BREAK","RETURN","PRINT",
+                              "READLN","THROW","TRY","CATCH","CASE","LITERAL","TRUE","FALSE",
+                              "ADDRESS","STAR","DOT","LBRACE","RBRACE","NOT","CHAR","QUOTE",
+                              "SIMPLEQUOTE","BACKSLASH","COLON","PLUS","MINUS","PIPE","SLASH",
+                              "PERCENT","AND","OR","POINTER","TYPEDEF","STRUCT","NUMINT",
+                              "NUMFLOAT","LITERALCHAR"};
+
+    return tokens[id];
+}
 
 #endif //COMPILADOR_2019_3_TOKEN_H
 #ifdef __cplusplus
