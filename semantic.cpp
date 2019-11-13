7896ccc6466cb424a0849c5f5621c143ee71b9b2
Arthur Gonze Machado <arthurgonze@gmail.com>
2019-11-13 18:01:04 -0300

    Classe do analisador semantico

---
+++ semantic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by caveira on 11/13/19.
+//
+
+#include "semantic.h"
---
+++ semantic.h
@@ -0,0 +1,13 @@
+//
+// Created by caveira on 11/13/19.
+//
+
+#ifndef COMPILADOR_2019_3_SEMANTIC_H
+#define COMPILADOR_2019_3_SEMANTIC_H
+
+class semantic
+{
+
+};
+
+#endif //COMPILADOR_2019_3_SEMANTIC_H
