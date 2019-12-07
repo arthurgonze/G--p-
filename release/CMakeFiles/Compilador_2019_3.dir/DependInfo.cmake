5024bbe79831f36293c7f25f262a1d0edf23593b
Arthur Gonze Machado <arthurgonze@gmail.com>
2019-12-07 18:29:17 -0300

    release

---
+++ release/CMakeFiles/Compilador_2019_3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/analyzer.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/analyzer.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/ast.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/ast.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/main.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/main.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/parser.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/parser.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/semantic.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/semantic.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/symbol.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/symbol.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/symboltable.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/symboltable.cpp.o"
+  "/home/caveira/Documents/GitProjects/Compilador-2019.3/visitor.cpp" "/home/caveira/Documents/GitProjects/Compilador-2019.3/release/CMakeFiles/Compilador_2019_3.dir/visitor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
