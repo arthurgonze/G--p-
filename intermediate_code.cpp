43913a09a6da27c1cbf9a95ea25274fcc105c195
Jorao Jr <joraojunior@ice.ufjf.br>
2019-12-09 15:44:01 -0300

    Add Temp, Temp_List, Label e Label_List

---
+++ intermediate_code.cpp
@@ -0,0 +1,168 @@
+#include <cstdio>
+#include <cstring>
+#include "intermediate_code.h"
+
+
+/*********************FRAGMENT**************************/
+
+Fragment *Fragment::getNext() const {
+    return next;
+}
+
+void Fragment::setNext(Fragment *next) {
+    Fragment::next = next;
+}
+
+/*********************PROCEDURE**************************/
+Frame *Procedure::getFrame() const {
+    return frame;
+}
+
+void Procedure::setFrame(Frame *frame) {
+    Procedure::frame = frame;
+}
+
+Stm *Procedure::getBody() const {
+    return body;
+}
+
+void Procedure::setBody(Stm *body) {
+    Procedure::body = body;
+}
+
+Procedure::~Procedure() {
+
+}
+
+Procedure::Procedure(Frame *frame, Stm *body) : frame(frame), body(body) {}
+
+/*********************LITERAL**************************/
+
+Literal::Literal(const char *literal) : literal(literal) {}
+
+const char *Literal::getLiteral() const {
+    return literal;
+}
+
+void Literal::setLiteral(const char *value) {
+    Literal::literal = literal;
+}
+
+/*********************VARIABLE**************************/
+
+Variable::Variable(int type, int nbytes) : type(type), nbytes(nbytes) {}
+
+Variable::~Variable() {
+
+}
+
+int Variable::getType() const {
+    return type;
+}
+
+void Variable::setType(int type) {
+    Variable::type = type;
+}
+
+int Variable::getNbytes() const {
+    return nbytes;
+}
+
+void Variable::setNbytes(int nbytes) {
+    Variable::nbytes = nbytes;
+}
+
+/*********************TEMP**************************/
+
+Temp::Temp() {
+    char name_aux[250];
+    sprintf(name_aux, "$%d", num_temps++);
+    this->name = new char[strlen(name_aux) + 1];
+    strcpy(this->name, name_aux);
+}
+
+Temp::Temp(const char *name) {
+    this->name = new char[strlen(name) + 1];
+    strcpy(this->name, name);
+}
+
+Temp::~Temp() {
+    // TODO Precisa decrementar o num_temps ?
+    this->name = nullptr;
+}
+
+char *Temp::getName() const {
+    return name;
+}
+
+char *Temp::toString() {
+    char *temp_name = new char[strlen("temp") + strlen(this->name) + 1];
+    strcpy(temp_name, "temp");
+    strcat(temp_name, this->name);
+    return temp_name;
+}
+
+/*********************TEMP_LIST**************************/
+
+
+TempList::TempList(Temp *temp, TempList *next) : temp(temp), next(next) {}
+
+TempList::~TempList() {
+    // TODO Precisa decrementar o num_temps ?
+    delete this->temp;
+    delete this->next;
+}
+
+Temp *TempList::getTemp() const {
+    return temp;
+}
+
+TempList *TempList::getNext() const {
+    return next;
+}
+
+
+/*********************LABEL**************************/
+
+Label::Label() {
+    char name_aux[250];
+    sprintf(name_aux, "Label$%d", num_labels++);
+    this->name = new char[strlen(name_aux) + 1];
+    strcpy(this->name, name_aux);
+}
+
+Label::Label(const char *name) {
+    this->name = new char[strlen(name) + 1];
+    strcpy(this->name, name);
+}
+
+Label::~Label() {
+    // TODO Precisa decrementar o num_labels ?
+    this->name = nullptr;
+}
+
+char *Label::getName() const {
+    return this->name;
+}
+
+char *Label::toString() {
+    return this->name;
+}
+
+/*********************LABEL_LIST**************************/
+
+LabelList::LabelList(Label *label, LabelList *next) : label(label), next(next) {}
+
+LabelList::~LabelList() {
+    // TODO Precisa decrementar o num_labels ?
+    delete this->label;
+    delete this->next;
+}
+
+Label *LabelList::getLabel() const {
+    return label;
+}
+
+LabelList *LabelList::getNext() const {
+    return next;
+}
---
+++ intermediate_code.h
@@ -0,0 +1,118 @@
+//
+// Created by jorao on 09/12/2019.
+//
+
+#ifndef COMPILADOR_2019_3_INTERMEDIATE_CODE_H
+#define COMPILADOR_2019_3_INTERMEDIATE_CODE_H
+
+
+
+int num_labels = 0;
+int num_temps = 0;
+
+class Fragment {
+private:
+    Fragment * next;
+public:
+    virtual ~Fragment() = default;
+    Fragment *getNext() const;
+    void setNext(Fragment *next);
+};
+
+class Frame{
+
+};
+
+class Stm{
+
+};
+
+
+class Procedure : public Fragment {
+private:
+    Frame *frame;
+    Stm * body;
+public:
+
+    Procedure(Frame *frame, Stm *body);
+    virtual ~Procedure();
+    Frame *getFrame() const;
+    void setFrame(Frame *frame);
+    Stm *getBody() const;
+    void setBody(Stm *body);
+};
+
+
+class Literal : public Fragment {
+private:
+    const char *literal;
+public:
+
+    Literal(const char *literal);
+    const char *getLiteral() const;
+    void setLiteral(const char *value);
+};
+
+class Variable : public Fragment {
+private:
+    int type;
+    int nbytes;
+public:
+
+    Variable(int type, int nbytes);
+    virtual ~Variable();
+    int getType() const;
+    void setType(int type);
+    int getNbytes() const;
+    void setNbytes(int nbytes);
+};
+
+class Temp {
+private:
+    char *name;
+public:
+
+    Temp();
+    Temp(const char *name);
+    virtual ~Temp();
+    char *getName() const;
+    char * toString();
+};
+
+class TempList {
+private:
+
+    Temp *temp;
+    TempList *next;
+public:
+
+    TempList(Temp *temp, TempList *next);
+    virtual ~TempList();
+    Temp *getTemp() const;
+    TempList *getNext() const;
+};
+
+class Label {
+private:
+    char *name;
+public:
+    Label();
+    Label(const char *name);
+    virtual ~Label();
+    char *toString();
+    char *getName() const;
+};
+
+class LabelList {
+private:
+    Label *label;
+    LabelList *next;
+public:
+    LabelList(Label *label, LabelList *next);
+    virtual ~LabelList();
+    Label *getLabel() const;
+    LabelList *getNext() const;
+};
+
+
+#endif //COMPILADOR_2019_3_INTERMEDIATE_CODE_H
